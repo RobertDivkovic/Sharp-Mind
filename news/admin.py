dcd33f68f3be034e36e5009e10a8138b9fa22b45
Robert Divkovic <robertdivkovic98@gmail.com>
2025-01-18 17:24:11 +0000

    Enhance post model with excerpt and comment with email

--- news/admin.py
+++ news/admin.py
@@ -5,15 +5,15 @@ from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "status", "created_on")
+    list_display = ("title", "slug", "status", "created_on", "author")
     list_filter = ("status", "created_on")
-    search_fields = ["title", "content"]
+    search_fields = ["title", "content", "excerpt"]
     prepopulated_fields = {"slug": ("title",)}
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("author", "body", "post", "created_on", "approved")
+    list_display = ("author", "email", "body", "post", "created_on", "approved")
     list_filter = ("approved", "created_on")
-    search_fields = ("author", "body")
+    search_fields = ("author","email", "body")
     actions = ["approve_comments"]
 
     def approve_comments(self, request, queryset):
--- news/models.py
+++ news/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -28,6 +29,7 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    email = models.EmailField(blank=True)
 
     class Meta:
         ordering = ["-created_on"]
