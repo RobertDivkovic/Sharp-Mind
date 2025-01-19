a0c14c948df41aa088e01a1935e71edc38023faa
Robert Divkovic <robertdivkovic98@gmail.com>
2025-01-19 00:34:18 +0000

    Add filtering posts by author and custom context to views

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostsByAuthor
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),  # Homepage URL
     path('<slug:slug>/', PostDetail.as_view(), name='post-detail'), # Detail view URL
+    path('author/<str:username>/', PostsByAuthor.as_view(), name='posts-by-author')
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your views here.
@@ -14,3 +15,18 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = "news/post_detail.html" # Path to the template for individual posts
+
+class PostsByAuthor(generic.ListView):
+    model = Post
+    template_name = "news/posts_by_author.html"
+    context_object_name = "posts"
+    paginate_by = 5
+
+    def get_queryset(self):
+        username = self.kwargs['username']  # Get the 'username' from the URL
+        return Post.objects.filter(author__username=username, status=1).order_by('-created_on')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = User.objects.get(username=self.kwargs['username'])
+        return context
