ebf99abf5f3948a9110cd370e11c49431429851a
Robert Divkovic <robertdivkovic98@gmail.com>
2025-01-22 19:19:37 +0000

    Responsive design changes

--- sharpmind/settings.py
+++ sharpmind/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["8000-robertdivkovi-sharpmind-i99sqn2um3k.ws.codeinstitute-ide.net", ".herokuapp.com"]
+ALLOWED_HOSTS = ["8000-robertdivkovi-sharpmind-4zsveewmclh.ws.codeinstitute-ide.net", ".herokuapp.com"]
 
 # Application definition
 
